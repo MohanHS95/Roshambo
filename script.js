8119e29fae9e13b1e38d468c7c9ce6f85a806e3c
MohanHS95 <mohanhsharan@gmail.com>
2024-11-06 13:23:07 -0500

    Add functionality to play 5 rounds of the game
    A new function called playGame was added and resulted in having to move
    the original playGame function inside it and duplicated 5 times with new
    humanSelection and computerSelection variables each time to store the
    result.
    
    An if-else statement was added to evaluate the player and computer
    scores to determine a winner of the game--variables which were also
    moved into the larger playGame function. The function was called
    at the end of the script.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getComputerChoice(min,max) {
 }
 
 function getHumanChoice() {
-    let HumanChoice = prompt("Rock, Paper, or Scissors?");
+    let HumanChoice = prompt("Lets play 5 rounds. Rock, Paper, or Scissors?");
 
     if (HumanChoice.toLowerCase() === "rock") {
         return "rock";
@@ -26,32 +26,33 @@ function getHumanChoice() {
 
     }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice,computerChoice) {
+    function playRound(humanChoice,computerChoice) {
 
-    if (humanChoice === computerChoice) {
+        if (humanChoice === computerChoice) {
         console.log(`Your choice (${humanChoice}) and the computers choice (${computerChoice}) are the same! Try again!`);
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
         console.log("Rock beats Scissors! You Win!");
         return humanScore ++;
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
         console.log("Paper beats Rock! You Lose!");
         return computerScore ++;
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
         console.log("Rock beats Scissors! You Lose!");
         return computerScore ++;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
         console.log("Scissors beats Paper! You win!");
         return humanScore ++;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
         console.log("Paper beats Rock! You win!");
         return humanScore ++;
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
         console.log("Scissors beats Paper! You lose!");
         return computerScore ++;
-     } else
+        } else
         console.log("Come on, pick one!");
 
 }
@@ -61,4 +62,39 @@ console.log(computerSelection);
 
 playRound(humanSelection,computerSelection);
 
+const humanSelection2 = getHumanChoice();
+const computerSelection2 = getComputerChoice(0,2);
+console.log(computerSelection2);
 
+playRound(humanSelection2,computerSelection2);
+
+const humanSelection3 = getHumanChoice();
+const computerSelection3 = getComputerChoice(0,2);
+console.log(computerSelection3);
+
+playRound(humanSelection3,computerSelection3);
+
+const humanSelection4 = getHumanChoice();
+const computerSelection4 = getComputerChoice(0,2);
+console.log(computerSelection4);
+
+playRound(humanSelection4,computerSelection4);
+
+const humanSelection5 = getHumanChoice();
+const computerSelection5 = getComputerChoice(0,2);
+console.log(computerSelection5);
+
+playRound(humanSelection5,computerSelection5);
+
+console.log(humanScore);
+console.log(computerScore);
+
+if (humanScore > computerScore) {
+    console.log("You've beat the computer. Congratulations!")
+} else if (humanScore < computerScore) {
+    console.log("Sorry, the computer got you this time. Refresh to try again!")
+} else
+    console.log("No winner. Try again!")
+}
+
+playGame();
